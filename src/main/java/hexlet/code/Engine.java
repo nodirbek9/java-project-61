553d3ccc1188b7bf5bc63c68bb0a3a8114b06013
user <user@MacBook-Air-user.local>
2025-11-01 18:25:54 +0300

    change Engine

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -7,16 +7,6 @@ import java.util.Scanner;
 public class Engine {
     private static final int ROUNDS_COUNT = 3;
 
-    public static void start(String description, String[] qa) {
-        String[][] questionsAndAnswers = new String[ROUNDS_COUNT][2];
-
-        for (int i = 0; i < 3; i++) {
-            questionsAndAnswers[i][0] = qa[0];
-            questionsAndAnswers[i][1] = qa[1];
-        }
-        Engine.run(description, questionsAndAnswers);
-    }
-
     public static void run(String description, String[][] questionsAndAnswers) {
         Scanner scanner = new Scanner(System.in);
         Cli cli = new Cli();
