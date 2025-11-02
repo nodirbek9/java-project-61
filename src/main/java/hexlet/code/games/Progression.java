102a7fd138cbe9a80f359297b97444b14f5fd288
user <user@MacBook-Air-user.local>
2025-11-02 15:42:36 +0300

    fixing SonarCloud warnings

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -2,10 +2,10 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class Progression {
-    private static final Random random = new Random();
+    private static final SecureRandom random = new SecureRandom();
     private static final int LENGTH = 10;
     private static final int ROUNDS_COUNT = 3;
 
