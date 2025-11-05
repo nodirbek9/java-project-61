ee8edd108e6cc361c328772dacbe208fc8131b26
user <user@MacBook-Air-user.local>
2025-11-05 20:31:54 +0300

    changes F

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 checkstyle {
     toolVersion = "10.12.4"
-    configFile = file("/Users/user/IdeaProjects/BrainGames/java-project-61/config/checkstyle/checkstyle.xml")
+    configFile = file("/java-project-61/config/checkstyle/checkstyle.xml")
 }
 
 sonar {
